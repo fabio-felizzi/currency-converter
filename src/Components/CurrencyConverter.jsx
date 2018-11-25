a65e7e22a643a515e14f8c82f39a05ba9bac6dc8
fabiofelizzi@gmail.com <fabiofelizzi@gmail.com>
2018-11-25 17:21:14 +0000

    adding markup and select handlers

--- src/Components/CurrencyConverter.jsx
+++ src/Components/CurrencyConverter.jsx
@@ -6,8 +6,11 @@ class CurrencyConverter extends Component {
   state = {
     fromCurrency: "USD",
     toCurrency: "GBP",
+    fromRate: "",
+    toRate: "",
     currencies: [],
-    rates: [],
+    amount: 1,
+    result: null,
   };
 
   componentDidMount() {
@@ -16,28 +19,67 @@ class CurrencyConverter extends Component {
         .then(response => {
             const xml = response.data;
             const currencies = [];
-            const rates = [];
-
             parseString(xml, (err, result) => {
               const currencyArr = result['gesmes:Envelope'].Cube[0].Cube[0].Cube;
               currencyArr.map((country) => {
-                currencies.push(country.$.currency);
-                rates.push(country.$.rate);
+                currencies.push(country.$);
               });
             });
-            this.setState({ currencies, rates });
+            this.setState({ currencies });
         })
         .catch(err => {
             console.log("Something went wrong!", err.message);
         });
   }
 
+  selectHandler = (event) => {
+    if (event.target.name === "from") {
+        this.setState({ fromCurrency: event.target.value })
+    }
+    if (event.target.name === "to") {
+        this.setState({ toCurrency: event.target.value })
+    }
+}
+
   
   render() {
+    const { amount, fromCurrency, toCurrency, result, currencies } = this.state;
+    console.log(currencies)
     return (
-      <div className="converter">
-        
-      </div>
+    <div className="converter">
+        <div className="Form">
+            <input
+                name="amount"
+                type="text"
+                value={amount}
+                onChange={event =>
+                    this.setState({ amount: event.target.value })
+                }
+            />
+            <select
+                name="from"
+                onChange={(event) => this.selectHandler(event)}
+                value={fromCurrency}
+            >
+                {currencies.map(cur => (
+                    <option key={cur.currency}>{cur.currency}</option>
+                ))}
+            </select>
+            <select
+                name="to"
+                onChange={(event) => this.selectHandler(event)}
+                value={toCurrency}
+            >
+                {currencies.map(cur => (
+                    <option key={cur.currency}>{cur.currency}</option>
+                ))}
+            </select>
+            <button>Convert</button>
+        </div>
+        {result && 
+            <h3>{result}</h3>
+        }
+    </div>
     );
   }
 }
