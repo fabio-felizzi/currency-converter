0218215bd9a503b76dfb6b3e38195c4c47239316
fabiofelizzi@gmail.com <fabiofelizzi@gmail.com>
2018-11-26 20:26:44 +0000

    added rough styling, added conversion funcitonality, wasn't able to figure out how to set the initial state for the currency rates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <CurrencyConverter />
+      <div className="app">
+        <CurrencyConverter />
+      </div>
     );
   }
 }
--- src/Components/CurrencyConverter.jsx
+++ src/Components/CurrencyConverter.jsx
@@ -1,87 +1,144 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import { parseString } from 'xml2js'; 
+import { parseString } from 'xml2js';
+
+import './CurrencyConverter.css';
 
 class CurrencyConverter extends Component {
-  state = {
-    fromCurrency: "USD",
-    toCurrency: "GBP",
-    fromRate: "",
-    toRate: "",
-    currencies: [],
-    amount: 1,
-    result: null,
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            fromCurrency: "USD",
+            toCurrency: "GBP",
+            fromRate: "",
+            toRate: "",
+            currencies: [],
+            toAmount: 1,
+            fromAmount: 1,
+            fromResult: null,
+            toResult: null,
+        };
 
-  componentDidMount() {
-    axios
-        .get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
-        .then(response => {
-            const xml = response.data;
-            const currencies = [];
-            parseString(xml, (err, result) => {
-              const currencyArr = result['gesmes:Envelope'].Cube[0].Cube[0].Cube;
-              currencyArr.map((country) => {
-                currencies.push(country.$);
-              });
+        this.submitHandler.bind(this);
+    }
+
+    componentDidMount() {
+        axios
+            .get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+            .then(response => {
+                const xml = response.data;
+                const currencies = [];
+                parseString(xml, (err, result) => {
+                    const currencyArr = result['gesmes:Envelope'].Cube[0].Cube[0].Cube;
+                    currencyArr.map((country) => {
+                        currencies.push(country.$);
+                    });
+                });
+
+                this.setState({
+                    currencies,
+                    fromCurrency: currencies[0].currency,
+                    toCurrency: currencies[1].currency,
+                });
+            })
+            .catch(err => {
+                console.log("Something went wrong!", err.message);
             });
-            this.setState({ currencies });
-        })
-        .catch(err => {
-            console.log("Something went wrong!", err.message);
-        });
-  }
-
-  selectHandler = (event) => {
-    if (event.target.name === "from") {
-        this.setState({ fromCurrency: event.target.value })
     }
-    if (event.target.name === "to") {
-        this.setState({ toCurrency: event.target.value })
-    }
-}
 
-  
-  render() {
-    const { amount, fromCurrency, toCurrency, result, currencies } = this.state;
-    console.log(currencies)
-    return (
-    <div className="converter">
-        <div className="Form">
-            <input
-                name="amount"
-                type="text"
-                value={amount}
-                onChange={event =>
-                    this.setState({ amount: event.target.value })
-                }
-            />
-            <select
-                name="from"
-                onChange={(event) => this.selectHandler(event)}
-                value={fromCurrency}
-            >
-                {currencies.map(cur => (
-                    <option key={cur.currency}>{cur.currency}</option>
-                ))}
-            </select>
-            <select
-                name="to"
-                onChange={(event) => this.selectHandler(event)}
-                value={toCurrency}
-            >
-                {currencies.map(cur => (
-                    <option key={cur.currency}>{cur.currency}</option>
-                ))}
-            </select>
-            <button>Convert</button>
-        </div>
-        {result && 
-            <h3>{result}</h3>
+    selectHandler = (event) => {
+        if (event.target.name === "from") {
+            this.setState({
+                fromCurrency: event.target.value,
+                fromRate: this.state.currencies.filter(currency =>
+                    currency.currency === this.state.fromCurrency),
+            })
         }
-    </div>
-    );
-  }
+        if (event.target.name === "to") {
+            this.setState({
+                toCurrency: event.target.value,
+                toRate: this.state.currencies.filter(currency =>
+                    currency.currency === this.state.toCurrency),
+            })
+        }
+    }
+
+    submitHandler() {
+        this.setState({
+            fromResult: this.state.fromRate[0].rate * this.state.fromAmount || this.state.fromRate[0].rate * this.state.toAmount,
+            toResult: this.state.toRate[0].rate * this.state.toAmount || this.state.toRate[0].rate * this.state.fromAmount,
+        });
+    }
+
+    render() {
+        const { fromAmount, toAmount, fromCurrency, toCurrency, fromRate, toRate, fromResult,
+            toResult, currencies } = this.state;
+
+        return (
+            <div className="converter">
+                <h1>Currency Converter</h1>
+                <div className="Form">
+                    <div className="valueWrapper">
+                        <p>{fromRate ? `1 ${fromCurrency} = ${fromRate[0].rate}` : null}</p>
+                        <select
+                            name="from"
+                            className="dropdown"
+                            onChange={(event) => this.selectHandler(event)}
+                            value={fromCurrency}
+                        >
+                            {currencies.map(cur => (
+                                <option key={cur.currency} rate={cur.rate}>{cur.currency}</option>
+                            ))}
+                        </select>
+                        <input
+                            name="amount"
+                            className="amount"
+                            type="text"
+                            value={fromAmount}
+                            onChange={event => {
+                                this.setState({ fromAmount: event.target.value })
+                                this.submitHandler()
+                            }}
+                        />
+                        <div className="result">
+                            {fromResult &&
+                                <h3 className="toResult">{`${fromCurrency} ${toResult}`}</h3>
+                            }
+                        </div>
+                    </div>
+                    <div className="valueWrapper">
+                        <p>{toRate ? `1 ${toCurrency} = ${toRate[0].rate}` : null}</p>
+                        <select
+                            name="to"
+                            className="dropdown"
+                            onChange={(event) => this.selectHandler(event)}
+                            value={toCurrency}
+                        >
+                            {currencies.map(cur => (
+                                <option key={cur.currency} rate={cur.rate}>{cur.currency}</option>
+                            ))}
+                        </select>
+                        <input
+                            name="amount"
+                            className="amount"
+                            type="text"
+                            value={toAmount}
+                            onChange={event => {
+                                this.setState({ toAmount: event.target.value })
+                                this.submitHandler()
+                            }}
+                        />
+                        <div className="result">
+                            {toResult &&
+                                <h3 className="fromResult">{`${toCurrency} ${fromResult}`}</h3>
+                            }
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default CurrencyConverter;
